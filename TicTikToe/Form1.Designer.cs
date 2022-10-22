2bce9ed73ff7a4752ec5ba3a442a77bebf615661
m0nst3r1989 <113396501+m0nst3r1989@users.noreply.github.com>
2022-10-22 11:27:48 -0400

    Add files via upload

---
+++ TicTikToe/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TicTikToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            this.firstCell = new System.Windows.Forms.Button();
+            this.SecondCell = new System.Windows.Forms.Button();
+            this.thirdCell = new System.Windows.Forms.Button();
+            this.cell4 = new System.Windows.Forms.Button();
+            this.cell5 = new System.Windows.Forms.Button();
+            this.cell6 = new System.Windows.Forms.Button();
+            this.cell7 = new System.Windows.Forms.Button();
+            this.cell8 = new System.Windows.Forms.Button();
+            this.cell9 = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstCell
+            // 
+            this.firstCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstCell.Location = new System.Drawing.Point(87, 84);
+            this.firstCell.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.firstCell.Name = "firstCell";
+            this.firstCell.Size = new System.Drawing.Size(152, 111);
+            this.firstCell.TabIndex = 0;
+            this.firstCell.UseVisualStyleBackColor = true;
+            this.firstCell.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SecondCell
+            // 
+            this.SecondCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondCell.Location = new System.Drawing.Point(255, 84);
+            this.SecondCell.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SecondCell.Name = "SecondCell";
+            this.SecondCell.Size = new System.Drawing.Size(152, 111);
+            this.SecondCell.TabIndex = 1;
+            this.SecondCell.UseVisualStyleBackColor = true;
+            this.SecondCell.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // thirdCell
+            // 
+            this.thirdCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdCell.Location = new System.Drawing.Point(415, 84);
+            this.thirdCell.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.thirdCell.Name = "thirdCell";
+            this.thirdCell.Size = new System.Drawing.Size(152, 111);
+            this.thirdCell.TabIndex = 2;
+            this.thirdCell.UseVisualStyleBackColor = true;
+            this.thirdCell.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // cell4
+            // 
+            this.cell4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell4.Location = new System.Drawing.Point(87, 206);
+            this.cell4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell4.Name = "cell4";
+            this.cell4.Size = new System.Drawing.Size(152, 111);
+            this.cell4.TabIndex = 3;
+            this.cell4.UseVisualStyleBackColor = true;
+            this.cell4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // cell5
+            // 
+            this.cell5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell5.Location = new System.Drawing.Point(255, 202);
+            this.cell5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell5.Name = "cell5";
+            this.cell5.Size = new System.Drawing.Size(152, 111);
+            this.cell5.TabIndex = 4;
+            this.cell5.UseVisualStyleBackColor = true;
+            this.cell5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // cell6
+            // 
+            this.cell6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell6.Location = new System.Drawing.Point(415, 202);
+            this.cell6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell6.Name = "cell6";
+            this.cell6.Size = new System.Drawing.Size(152, 111);
+            this.cell6.TabIndex = 5;
+            this.cell6.UseVisualStyleBackColor = true;
+            this.cell6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // cell7
+            // 
+            this.cell7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell7.Location = new System.Drawing.Point(87, 324);
+            this.cell7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell7.Name = "cell7";
+            this.cell7.Size = new System.Drawing.Size(152, 111);
+            this.cell7.TabIndex = 6;
+            this.cell7.UseVisualStyleBackColor = true;
+            this.cell7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // cell8
+            // 
+            this.cell8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell8.Location = new System.Drawing.Point(255, 324);
+            this.cell8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell8.Name = "cell8";
+            this.cell8.Size = new System.Drawing.Size(152, 111);
+            this.cell8.TabIndex = 7;
+            this.cell8.UseVisualStyleBackColor = true;
+            this.cell8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // cell9
+            // 
+            this.cell9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell9.Location = new System.Drawing.Point(415, 324);
+            this.cell9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cell9.Name = "cell9";
+            this.cell9.Size = new System.Drawing.Size(152, 111);
+            this.cell9.TabIndex = 8;
+            this.cell9.UseVisualStyleBackColor = true;
+            this.cell9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(87, 458);
+            this.resultBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(479, 22);
+            this.resultBox.TabIndex = 9;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(87, 518);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(184, 42);
+            this.newGameButton.TabIndex = 10;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(388, 518);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(169, 42);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 655);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.cell9);
+            this.Controls.Add(this.cell8);
+            this.Controls.Add(this.cell7);
+            this.Controls.Add(this.cell6);
+            this.Controls.Add(this.cell5);
+            this.Controls.Add(this.cell4);
+            this.Controls.Add(this.thirdCell);
+            this.Controls.Add(this.SecondCell);
+            this.Controls.Add(this.firstCell);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button firstCell;
+        private System.Windows.Forms.Button SecondCell;
+        private System.Windows.Forms.Button thirdCell;
+        private System.Windows.Forms.Button cell4;
+        private System.Windows.Forms.Button cell5;
+        private System.Windows.Forms.Button cell6;
+        private System.Windows.Forms.Button cell7;
+        private System.Windows.Forms.Button cell8;
+        private System.Windows.Forms.Button cell9;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
---
+++ TicTikToe/Form1.cs
@@ -0,0 +1,792 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTikToe
+{
+    public partial class Form1 : Form
+    {
+        int checkPlayer = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (checkPlayer == 0)
+            {
+
+                firstCell.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                firstCell.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+
+            firstCell.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            //test the players chance
+
+            if (checkPlayer == 0)
+            {
+
+                SecondCell.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                SecondCell.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+            //already selected cells should be disbled
+
+            SecondCell.Enabled = false;
+
+            //check for winners
+            winCheck();
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            //test the winners
+
+            if (checkPlayer == 0)
+            {
+
+                thirdCell.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                thirdCell.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+            thirdCell.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            //test whose turn
+
+            if (checkPlayer == 0)
+            {
+
+                cell4.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell4.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+            //already selected button, so can't change
+
+            cell4.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            //check the turn and assign X or 0
+
+            if (checkPlayer == 0)
+            {
+
+                cell5.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell5.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+
+            cell5.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        //to perform cell6 click
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            //check the turn and assign X or 0
+            if (checkPlayer == 0)
+            {
+
+                cell6.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell6.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+
+            cell6.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            //check the turn and assign X or 0
+
+            if (checkPlayer == 0)
+            {
+
+                cell7.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell7.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+
+            cell7.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+
+            //check the turn and assign X or 0
+            if (checkPlayer == 0)
+            {
+
+                cell8.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell8.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+            cell8.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            //check the turn and assign X or 0
+
+            if (checkPlayer == 0)
+            {
+
+                cell9.Text = "X";
+
+                checkPlayer++;
+
+            }
+
+            else if (checkPlayer == 1)
+            {
+
+                cell9.Text = "O";
+
+                checkPlayer--;
+
+            }
+
+
+            cell9.Enabled = false;
+
+
+            winCheck();
+
+        }
+
+        void winCheck()
+        {
+
+            //check the winner
+
+            if (firstCell.Text != "" && SecondCell.Text != "" && thirdCell.Text != "" &&
+
+            cell4.Text != "" && cell5.Text != "" && cell6.Text != "" &&
+
+            cell7.Text != "" && cell8.Text != "" && cell9.Text != "")
+            {
+
+                resultBox.Text = "Game Tied";
+
+            }
+
+            //assign the letter
+
+            if (firstCell.Text == "X" && cell5.Text == "X" && cell9.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (thirdCell.Text == "X" && cell5.Text == "X" && cell7.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            //check win for x
+
+            if (firstCell.Text == "X" && SecondCell.Text == "X" && thirdCell.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (cell4.Text == "X" && cell5.Text == "X" && cell6.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (cell7.Text == "X" && cell8.Text == "X" && cell9.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            //winCheck columns for X
+
+            if (firstCell.Text == "X" && cell4.Text == "X" && cell7.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (SecondCell.Text == "X" && cell5.Text == "X" && cell8.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (thirdCell.Text == "X" && cell6.Text == "X" && cell9.Text == "X")
+            {
+
+                resultBox.Text = "Player X wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            //check whether 0 wins
+
+            if (firstCell.Text == "O" && cell5.Text == "O" && cell9.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (thirdCell.Text == "O" && cell5.Text == "O" && cell7.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            //winCheck rows for O
+
+            if (firstCell.Text == "O" && SecondCell.Text == "O" && thirdCell.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (cell4.Text == "O" && cell5.Text == "O" && cell6.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (cell7.Text == "O" && cell8.Text == "O" && cell9.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            //check for winner 0
+
+            if (firstCell.Text == "O" && cell4.Text == "O" && cell7.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (SecondCell.Text == "O" && cell5.Text == "O" && cell8.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+            if (thirdCell.Text == "O" && cell6.Text == "O" && cell9.Text == "O")
+            {
+
+                resultBox.Text = "Player O wins";
+
+                firstCell.Enabled = false;
+
+                SecondCell.Enabled = false;
+
+                thirdCell.Enabled = false;
+
+                cell4.Enabled = false;
+
+                cell5.Enabled = false;
+
+                cell6.Enabled = false;
+
+                cell7.Enabled = false;
+
+                cell8.Enabled = false;
+
+                cell9.Enabled = false;
+
+            }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            firstCell.Text = "";
+
+            firstCell.Enabled = true;
+
+            SecondCell.Text = "";
+
+            SecondCell.Enabled = true;
+
+            thirdCell.Text = "";
+
+            thirdCell.Enabled = true;
+
+            cell4.Text = "";
+
+            cell4.Enabled = true;
+
+            cell5.Text = "";
+
+            cell5.Enabled = true;
+
+            cell6.Text = "";
+
+            cell6.Enabled = true;
+
+            cell7.Text = "";
+
+            cell7.Enabled = true;
+
+            cell8.Text = "";
+
+            cell8.Enabled = true;
+
+            cell9.Text = "";
+
+            cell9.Enabled = true;
+
+            resultBox.Text = "";
+
+            checkPlayer = 0;
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
+
